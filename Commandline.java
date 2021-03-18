da5a24c8709b8661afffad95ed1d1cf8e81ef458
Himanshu Sajwan <57579061+HimanshuSajwan911@users.noreply.github.com>
2021-03-18 20:47:12 +0530

    Commandline.java
    Experiment 1.
    Taking input from Command line and convert objects into primitive data type

---
+++ Commandline.java
@@ -0,0 +1,14 @@
+package experiment;
+
+public class Commandline {
+
+	public static void main(String[] args){
+		
+		System.out.println("Name: " + args[0]);
+		System.out.println("UniversityRollNo: " + Integer.parseInt(args[2]));
+		System.out.println("Course: " + args[1]);
+		System.out.println("Semester: " + Integer.parseInt(args[3]));
+		
+	}
+	
+}
